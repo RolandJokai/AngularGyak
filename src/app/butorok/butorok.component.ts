c5d72ddfd3e1963dfcd0e2ffd6c85fa7c3324777
jokairoland <jokairoland@gmail.com>
2024-10-04 14:15:07 +0200

    verzio 1.1

--- src/app/butorok/butorok.component.ts
+++ src/app/butorok/butorok.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
+export interface Butor {
+  cikkszam: string;
+  megnevezes: string;
+  ar: number;
+  db: number;
+}
+
 @Component({
   selector: 'app-butorok',
   templateUrl: './butorok.component.html',
-  styleUrl: './butorok.component.css'
+  styleUrls: ['./butorok.component.css']
 })
 export class ButorokComponent {
+  cikkszam: string = "";
+  megnevezes: string = "";
+  ar: number = 0;
+  db: number = 0;
 
+  butorok: Butor[] = [];
+
+  // A felvitel függvény frissítése
+  felvitel(): void {
+    // Új butor objektumot hozunk létre az aktuális mezőértékek alapján
+    const ujButor: Butor = {
+      cikkszam: this.cikkszam,
+      megnevezes: this.megnevezes,
+      ar: this.ar,
+      db: this.db
+    };
+
+    // Hozzáadjuk a butorok tömbhöz
+    this.butorok.push(ujButor);
+
+    // Űrlap mezők alaphelyzetbe állítása
+    this.cikkszam = "";
+    this.megnevezes = "";
+    this.ar = 0;
+    this.db = 0;
+  }
 }
+
+
